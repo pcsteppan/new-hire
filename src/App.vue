d60cff234539783318e350e3a455681daf5026ac
Patrick Steppan <patrick.steppan@gmail.com>
2018-09-04 15:24:18 -0500

    added form component and revised icon component

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,44 @@
 <template>
   <div id="app">
-    <icon-button name="edit"/>
-    <icon-button name="update"/>
-    <icon-button name="remove"/>
-    <icon-button name="add"/>
+    <NewhireFormSection/>
   </div>
 </template>
 
 <script>
+import NewhireFormSection from '@/components/NewhireFormSection.vue'
 import IconButton from '@/components/IconButton.vue'
 
 export default {
   name: 'app',
 
   components: {
-    IconButton
+    IconButton,
+    NewhireFormSection
   }
 }
 </script>
 
 <style>
+* {
+  margin: 0;
+  padding: 0;
+  list-style: none;
+}
+
+html {
+  overflow: hidden;
+}
+
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
+
+  display: grid;
+  grid-template-columns: 400px minmax(700px, 1fr);
+  grid-template-rows: 1fr 320px;
+  font-family: 'Open Sans', sans-serif;
+  height: 100vh;
+  overflow: hidden;
 }
 </style>
